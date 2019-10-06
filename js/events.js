a3c55fdf63af0263769dae26ec86c36b567574dc
Georgi Petrushev <figoto660@gmail.com>
2019-10-06 16:27:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,38 +1,45 @@
 //define functions here
 
+
+
+
 $(document).ready(function(){
 
 
- $("h1").on("mouseenter",function() {
+     $("h1").on("mouseenter",function() {
             alert("Hello, world!");
-  });
+      });
+      
           
- $(document).on('keypress', function(key) {
-  if (key.which == 13){
-    alert('enter was pressed');
-    }
-  });     
+     $(document).on('keypress', function(key) {
+        if (key.which == 13){
+        alert('enter was pressed');
+        }
+      });   
+       
           
-  $(document).on('keyup', function(key) {
-  if (key.which == 82){
-    alert('r was pressed');
-  }
-});
+      $(document).on('keyup', function(key) {
+       if (key.which == 82){
+      alert('r was pressed');
+      }
+      });
+      
   
-  $(document).on('keydown', function(key) {
-  if(key.which == 83){
-      alert('s was pressed');
-  }
-});
+      $(document).on('keydown', function(key) {
+         if(key.which == 83){
+        alert('s was pressed');
+        }
+      });
+      
 
-$("form").on("submit", function() {
-  if ($( "input:first" ).val() === "correct") {
-    alert('your form is going to be submitted now');
-    return;
-  }
-  alert("you entered the wrong value");
-  return;
-});
+      $("form").on("submit", function() {
+        if ($( "input:first" ).val() === "correct") {
+        alert('your form is going to be submitted now');
+        return;
+        }
+        alert("you entered the wrong value");
+        return;
+      });
 
 
   
