9b233a41de34748278cb7ca69c5c16407aae48b8
Georgi Petrushev <figoto660@gmail.com>
2019-10-06 16:46:59 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -6,9 +6,9 @@ function getIt(){
   }
 
 function frameIt() {
-   $('img').on('load', function(){
-   $('img').addClass('tasty');
-  })
+  $('img').on('load', function() {
+    $('img').addClass('tasty');
+  });
 }
 
 
