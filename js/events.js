d81faaf244cfa426511dcc6bb8cf601cf52acf68
Georgi Petrushev <figoto660@gmail.com>
2019-10-06 16:19:30 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -15,9 +15,5 @@ $(document).ready(function(){
           
 });
 
- $(document).on('keypress', function(key) {
-  if (key.which == 13){
-    alert('enter was pressed');
-  }
-});         
+      
         
