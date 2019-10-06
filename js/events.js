f555932e2a1965665a25e98a8c2bcd26038aaad8
Georgi Petrushev <figoto660@gmail.com>
2019-10-06 16:37:00 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -16,46 +16,7 @@ function frameIt(){
 $(document).ready(function(){
     getIn();
 
-     $("h1").on("mouseenter",function() {
-            alert("Hello, world!");
-      });
-      
-          
-     $(document).on('keypress', function(key) {
-        if (key.which == 13){
-        alert('enter was pressed');
-        }
-      });   
-       
-          
-      $(document).on('keyup', function(key) {
-       if (key.which == 82){
-      alert('r was pressed');
-      }
-      });
-      
-  
-      $(document).on('keydown', function(key) {
-         if(key.which == 83){
-        alert('s was pressed');
-        }
-      });
-      
-
-      $("form").on("submit", function() {
-        if ($( "input:first" ).val() === "correct") {
-        alert('your form is going to be submitted now');
-        return;
-        }
-        alert("you entered the wrong value");
-        return;
-      });
-
-
-  
-  
-  
-  
+    
   
   
   
